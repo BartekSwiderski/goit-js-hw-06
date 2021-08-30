d29386dbe3d964b06472ddc97f00b2eaf3814105
Bartek Świderski <bartekzg@gmail.com>
2021-08-30 18:33:47 +0200

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const qsa = (selector) => document.querySelectorAll(selector);
+const items = qsa("li.item");
+  
+  console.log("Number of categories: " + items.length);
+  [...items].forEach((item) => {
+    console.log("Category: " + item.children[0].textContent);
+    console.log("Elements: " + item.children[1].children.length)})
