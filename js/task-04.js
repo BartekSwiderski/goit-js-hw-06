329421d64f15180115efc588d694dbd39f49f790
Bartek Świderski <bartekzg@gmail.com>
2021-08-30 22:12:02 +0200

    4

--- js/task-04.js
+++ js/task-04.js
@@ -1,21 +1,15 @@
 const qs = (selector) => document.querySelector(selector);
-
 const value = qs("#value");
+let  counterValue = 0;
 
-let counterValue = 35;
-
-const counterChange = (changeType) => {
-    if (changeType === "increment") counterValue++;
-    if (changeType === "decrement") counterValue--;
-    value.innerText = counterValue
-  }
+const btnClick = (a) => {
+   if (a ==='minus') counterValue--;
+   if (a ==='plus') counterValue++;
+    value.innerHTML = counterValue
+};
 
 const minusBtn = qs('[data-action="decrement"]');
-
-minusBtn.addEventListener("click", counterChange("decrement"));
-
 const plusBtn = qs('[data-action="increment"]');
 
-plusBtn.addEventListener("click", counterChange("increment"));
-
-
+minusBtn.addEventListener("click", (a) => btnClick('minus'));
+plusBtn.addEventListener("click", (a) => btnClick('plus'));
